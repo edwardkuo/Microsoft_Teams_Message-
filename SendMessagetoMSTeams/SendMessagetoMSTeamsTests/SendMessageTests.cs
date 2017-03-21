87ef52cac6a233cf57b83f7cad91d1be3a8d9625
EDWARDPC\ChiaChi <www@ww.w>
2017-03-21 23:11:37 +0800

    OK

--- SendMessagetoMSTeams/SendMessagetoMSTeamsTests/SendMessageTests.cs
+++ SendMessagetoMSTeams/SendMessagetoMSTeamsTests/SendMessageTests.cs
@@ -11,13 +11,13 @@ namespace SendMessagetoMSTeams.Tests
     [TestClass()]
     public class SendMessageTests
     {
-        private string MSTeamsChannel = "Channel URL";
+        private string MSTeamsChannel = "https://outlook.office.com/webhook/2cd44f77-d9f6-4de1-8baf-6c761390d8fa@ae4d8d00-0b04-4be4-80e7-9a22cbf55fe7/IncomingWebhook/4c0729ef74fd42829d03e21fa1302ee3/ddf6cd19-24e2-4f4e-b073-d18ee0f82602";
 
         [TestMethod()]
         public void InfoTest()
         {
             SendMessage MsTeamsmessage = new SendMessage(MSTeamsChannel, "Demo");
-            MsTeamsmessage.Info("VS Everywhere Demo", "Info", "台中");
+            MsTeamsmessage.Info("VS Everywhere Demo", "Info", "Good");
         }
     }
 }
